ef4d8289fde9ee415b118bd2db98d171cd6d8336
avishaneu <avisha.neu@protonmail.com>
2017-01-24 08:32:17 +0300

    Initial commit

---
+++ src/main/java/com/avisha_neu/basics/opengl/Main.java
@@ -0,0 +1,10 @@
+package com.avisha_neu.basics.opengl;
+
+/**
+ * Created by avisha.neu on 24.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
+}
