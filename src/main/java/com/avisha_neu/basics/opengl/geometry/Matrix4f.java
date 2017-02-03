4e359aad2397e43bd63c4c4ec30b923f587cf929
avishaneu <avisha.neu@protonmail.com>
2017-02-03 09:23:06 +0300

    \some basic multiplications

--- src/main/java/com/avisha_neu/basics/opengl/geometry/Matrix4f.java
+++ src/main/java/com/avisha_neu/basics/opengl/geometry/Matrix4f.java
@@ -5,31 +5,10 @@ package com.avisha_neu.basics.opengl.geometry;
  */
 public class Matrix4f {
 
-    /*private float m00;
-    private float m10;
-    private float m20;
-    private float m30;
-
-    private float m01;
-    private float m11;
-    private float m21;
-    private float m31;
-
-    private float m02;
-    private float m12;
-    private float m22;
-    private float m32;
-
-    private float m03;
-    private float m13;
-    private float m23;
-    private float m33;*/
-
-    Vector4f firstRow;
-    Vector4f secondRow;
-    Vector4f thirdRow;
-    Vector4f forthRow;
-
+    private Vector4f firstRow;
+    private Vector4f secondRow;
+    private Vector4f thirdRow;
+    private Vector4f forthRow;
 
     public Matrix4f() {
     }
@@ -75,5 +54,55 @@ public class Matrix4f {
         resultVector.setZ(this.thirdRow.multiplyScalar(vector));
         resultVector.setW(this.forthRow.multiplyScalar(vector));
         return resultVector;
+    }
+
+    private Vector4f getFirstCol() {
+        return new Vector4f(this.firstRow.getX(),
+                this.secondRow.getX(), this.thirdRow.getX(), this.forthRow.getX());
+    }
+
+    private Vector4f getSecondCol() {
+        return new Vector4f(this.firstRow.getY(),
+                this.secondRow.getY(), this.thirdRow.getY(), this.forthRow.getY());
+    }
+
+    private Vector4f getThirdCol() {
+        return new Vector4f(this.firstRow.getZ(),
+                this.secondRow.getZ(), this.thirdRow.getZ(), this.forthRow.getZ());
+    }
+
+    private Vector4f getForthCol() {
+        return new Vector4f(this.firstRow.getW(),
+                this.secondRow.getW(), this.thirdRow.getW(), this.forthRow.getW());
+    }
+
+
+    public Matrix4f multiply(Matrix4f matrix) {
+        Vector4f resultFirstRow = new Vector4f();
+        resultFirstRow.setX(this.firstRow.multiplyScalar(matrix.getFirstCol()));
+        resultFirstRow.setY(this.firstRow.multiplyScalar(matrix.getSecondCol()));
+        resultFirstRow.setZ(this.firstRow.multiplyScalar(matrix.getThirdCol()));
+        resultFirstRow.setW(this.firstRow.multiplyScalar(matrix.getForthCol()));
+
+        Vector4f resultSecondRow = new Vector4f();
+        resultSecondRow.setX(this.secondRow.multiplyScalar(matrix.getFirstCol()));
+        resultSecondRow.setY(this.secondRow.multiplyScalar(matrix.getSecondCol()));
+        resultSecondRow.setZ(this.secondRow.multiplyScalar(matrix.getThirdCol()));
+        resultSecondRow.setW(this.secondRow.multiplyScalar(matrix.getForthCol()));
+
+        Vector4f resultThirdRow = new Vector4f();
+        resultThirdRow.setX(this.thirdRow.multiplyScalar(matrix.getFirstCol()));
+        resultThirdRow.setY(this.thirdRow.multiplyScalar(matrix.getSecondCol()));
+        resultThirdRow.setZ(this.thirdRow.multiplyScalar(matrix.getThirdCol()));
+        resultThirdRow.setW(this.thirdRow.multiplyScalar(matrix.getForthCol()));
+
+        Vector4f resultForthRow = new Vector4f();
+        resultThirdRow.setX(this.forthRow.multiplyScalar(matrix.getFirstCol()));
+        resultThirdRow.setY(this.forthRow.multiplyScalar(matrix.getSecondCol()));
+        resultThirdRow.setZ(this.forthRow.multiplyScalar(matrix.getThirdCol()));
+        resultThirdRow.setW(this.forthRow.multiplyScalar(matrix.getForthCol()));
+
+        return new Matrix4f(resultFirstRow, resultSecondRow, resultThirdRow, resultForthRow);
+
     }
 }
